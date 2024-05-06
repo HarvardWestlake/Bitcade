c70c7e67a3157dbcd3b880857aff40ef9d9e0ca0
Jingjing <chrystalduan@gmail.com>
2024-05-05 17:14:46 -0700

    worked on page javascript

--- ui/selectionScreen.js
+++
@@ -1,6 +0,0 @@
-/***
- * create a dark (black) background
- * text will be neon maybe yellow or multicolored
- * should have big title "WYRM" on the top 
- * below, there will be all the games (with photo and name) and their description underneath
- */
