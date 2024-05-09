2c3a7f877cc0ecbdfe978a561a5cf4ca62b908ac
David <dchen1561@gmail.com>
2024-05-09 00:13:41 -0700

    Basic tinkering

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header'; // make sure to import your Header component
 import { WalletProvider } from './components/WalletContext'; // Import the provider
-import ExampleGameComponent from './games/ExampleGameComponent'; // Import the game component
+import Joining from './games/Joining';
 
 // Update the contract address here
 const exampleContractAddress = "0x51C72848c68a965f66FA7a88855F9f7784502a7F";
@@ -16,7 +16,8 @@ function App() {
           <div className="side-panel" style={{ flex: '15%' }}></div>
           <div className="main-panel" style={{ flex: '70%' }}>
             {/* Main content goes here */}
-            <ExampleGameComponent contractAddress={exampleContractAddress} />
+            {/* <ExampleGameComponent contractAddress={exampleContractAddress} /> */}
+            <Joining contractAddress={exampleContractAddress}/>
           </div>
           <div className="side-panel" style={{ flex: '15%' }}></div>
         </div>
---
+++ ui/src/games/Joining.js
@@ -0,0 +1,41 @@
+import { ethers } from 'ethers';
+import React, { useState } from 'react';
+import { useWallet } from '../components/WalletContext';
+
+const Joining = ({ contractAddress }) => {
+    const { walletAddress, contracts } = useWallet();
+    const [gameResult, setGameResult] = useState('');
+    const [loading, setLoading] = useState(false);
+    const gameContract = contracts && contracts.ExampleGame
+    ? new ethers.Contract(contractAddress, contracts.ExampleGame, walletAddress)
+    : null;
+
+    const joinGame = async () => {
+        if (!gameContract) return;
+        setLoading(true);
+        try {
+            // const joinTx = await gameContract.play();
+            // const receipt = await joinTx.wait();
+            // const joinResult = receipt.events?.filter((x) => x.event === 'Join')[0].args.result;
+            setGameResult(69);
+        } catch (error) {
+            console.error('Error joining game:', error);
+            alert('Failed to join game');
+        }
+        setLoading(false);
+    };
+    return (
+        <div className="example-game-component" style={{ width: "300px", border: "1px solid white", padding: "10px" }}>
+            <h3>Example Game</h3>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <button onClick={joinGame} disabled={!gameContract}>Play Game</button>
+                    <p>Game Result: {gameResult}</p>
+                </>
+            )}
+        </div>
+    );
+}
+export default Joining;
