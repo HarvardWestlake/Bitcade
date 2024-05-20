b6278081d3c6d8cfef042ac389233ce35a9b484d
Chris Weng <chris.r.weng@gmail.com>
2024-05-20 10:41:49 -0700

    Final Commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./Header"; // make sure to import your Header component
 import { WalletProvider } from "./components/WalletContext"; // Import the provider
 import ExampleGameComponent from "./games/ExampleGameComponent"; // Import the game component
-import LeaderBoard from "./components/LeaderBoard";
+// import LeaderBoard from "./components/LeaderBoard"; // Uncomment this to view LeaderBoard
 
 // Update the contract address here
 const exampleContractAddress = "0x51C72848c68a965f66FA7a88855F9f7784502a7F";
@@ -20,7 +20,12 @@ function App() {
           <div className="main-panel" style={{ flex: "70%" }}>
             {/* Main content goes here */}
             {/* <ExampleGameComponent contractAddress={exampleContractAddress} /> */}
-            <LeaderBoard></LeaderBoard>
+
+            {/* Front end Leaderboard code below: UNCOMMENT LINE BELOW TO VIEW FRONT END LEADERBOARD*/}
+
+            {/* <LeaderBoard></LeaderBoard> */}
+
+            {/* LeaderBoard Front End made by Chris Weng, Ronan Valle, Aidan Michaelson*/}
           </div>
           <div className="side-panel" style={{ flex: "15%" }}></div>
         </div>
