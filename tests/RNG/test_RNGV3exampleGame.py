cecf4f4a4a8a275eb75205dce9487209de320140
Andrew Theiss <andrew.theiss@gmail.com>
2024-05-22 07:51:22 -0700

    Update tests to be deterministic and remove excess INFO level logging

--- tests/RNG/test_RNGV3exampleGame.py
+++ tests/RNG/test_RNGV3exampleGame.py
@@ -1,10 +1,12 @@
 from ape import accounts, project
 from ape import chain
 
+# Tests Play
+# To view output, uncomment 'print' from the contracts
 def test_play(accounts):
     owner = accounts[0]
     RNGcontract = project.RNGV3.deploy(sender=owner)
-    diceContract = project.RNGV3ExampleGame.deploy(RNGcontract, sender=owner, value=100000000000000000000000)
+    diceContract = project.RNGV3ExampleGame.deploy(RNGcontract, sender=owner, value=2000000000000000000)
 
     player = accounts[1]
 
@@ -13,38 +15,40 @@ def test_play(accounts):
     #stake and pay for your first bet
     startGameReceipt = diceContract.startGame(sender=player, value=1000000000000000000)
 
-    print("\n\n\n\nSTART GAME")
-    print("contract after bet:")
-    print(diceContract.balance)
+    ogPlayerBalance = player.balance
+    assert diceContract.balance == 3000000000000000000
+    # print("\n\n\n\nSTART GAME")
+    # print("contract after bet:")
+    # print(diceContract.balance)
 
-    print("player after bet:")
-    print(player.balance)
+    # print("player after bet:")
+    # print(player.balance)
 
     #Play the game 9 times
     for i in range(9):
         chain.mine(3)
-        betReceipt = diceContract.playGame((i + 1) * 10, sender=player, value=1000000000000000000 * (i + 1))
+        betReceipt = diceContract.playGame((i + 1) * 10, sender=player, value=1000000000000000 * (i + 1))
 
-        print("\n\n\nPLAY GAME")
+        # print("\n\n\nPLAY GAME")
         for log in diceContract.Play.from_receipt(betReceipt):
-            print(log.randomNumber)
-            print((i + 1) * 10)
+            # print(log.randomNumber)
+            # print((i + 1) * 10)
             assert log.randomNumber <= 100
             assert log.randomNumber >= 0
-            print(log.result) 
+            # print(log.result) 
             if (log.randomNumber >= (i + 1) * 10):
                 assert log.result == False
             else:
                 assert log.result == True
-            print(log.winnings) 
-            print(log.multiplier)
-            print(log.player)
+            # print(log.winnings) 
+            # print(log.multiplier)
+            # print(log.player)
         
-            print("contract after bet:")
-            print(diceContract.balance)
+            # print("contract after bet:")
+            # print(diceContract.balance)
 
-            print("player after bet:")
-            print(player.balance)
+            # print("player after bet:")
+            # print(player.balance)
 
     chain.mine(3)
 
@@ -53,21 +57,26 @@ def test_play(accounts):
     #staked your bet on the previous turn
     betReceipt = diceContract.cashOut(99, sender=player)
 
-    print("\n\n\nEND GAME")
+    # print("\n\n\nEND GAME")
     for log in diceContract.Play.from_receipt(betReceipt):
-        print(log.randomNumber)
-        print((i + 1) * 10)
-        print(log.result) 
-        print(log.winnings) 
-        print(log.multiplier)
-        print(log.player)
+        assert 1 == 1 # placeholder for loop interior
+        # print(log.randomNumber)
+        # print((i + 1) * 10)
+        # print(log.result) 
+        # print(log.winnings) 
+        # print(log.multiplier)
+        # print(log.player)
 
-    print("contract after bet:")
-    print(diceContract.balance)
+    # print("contract after bet:")
+    # print(diceContract.balance)
 
-    print("player after bet:")
-    print(player.balance)
+    # print("player after bet:")
+    # print(player.balance)
 
-    assert 1 > 2
+    # Make sure money switched hands during the betting
+    assert player.balance != 3000000000000000000
+    assert player.balance != ogPlayerBalance
+    assert (diceContract.balance > player.balance) or (diceContract.balance < player.balance)
+    assert diceContract.balance != 3000000000000000000
 
-    print("Test passed, all bets worked")
+    # print("Test passed, all bets worked")
