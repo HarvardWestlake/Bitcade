37125111a93442ac819f000239e68ee89d641425
Grady <gradyramberg@gmail.com>
2024-05-23 12:52:52 -0700

    Creating New Branch

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,25 +3,31 @@ import './App.css';
 import Header from './Header'; // make sure to import your Header component
 import { WalletProvider } from './components/WalletContext'; // Import the provider
 import ExampleGameComponent from './games/ExampleGameComponent'; // Import the game component
+import RacingGame from './games/RacingGameUI'; // Import the game component
 import RockPaperScissors from './games/RockPaperScissors';
 
 // Update the contract address here
-const exampleContractAddress = "0x51C72848c68a965f66FA7a88855F9f7784502a7F";
-const testingRPCAdress = "0x65DfcD089DC1135F687dA33a05C66Ec6B78C802F"
+const exampleContractAddress = '0x51C72848c68a965f66FA7a88855F9f7784502a7F';
+const racingGameAddress = '0x51C72848c68a965f66FA7a88855F9f7784502a7F';
+const testingRPCAdress = '0x65DfcD089DC1135F687dA33a05C66Ec6B78C802F';
 
 function App() {
   return (
-    <WalletProvider> {/* Wrap the entire application with WalletProvider */}
-      <div className="App">
+    <WalletProvider>
+      {' '}
+      {/* Wrap the entire application with WalletProvider */}
+      <div className='App'>
         <Header />
-        <div className="content">
-          <div className="side-panel" style={{ flex: '15%' }}></div>
-          <div className="main-panel" style={{ flex: '70%' }}>
+        <div className='content'>
+          <div className='side-panel' style={{ flex: '15%' }}></div>
+          <div className='main-panel' style={{ flex: '70%' }}>
             {/* Main content goes here */}
             {/* <ExampleGameComponent contractAddress={exampleContractAddress} /> */}
-            <RockPaperScissors contractAddress={testingRPCAdress}/>
+
+            <RockPaperScissors contractAddress={testingRPCAdress} />
+            <RacingGame contractAddress={racingGameAddress} />
           </div>
-          <div className="side-panel" style={{ flex: '15%' }}></div>
+          <div className='side-panel' style={{ flex: '15%' }}></div>
         </div>
       </div>
     </WalletProvider>
