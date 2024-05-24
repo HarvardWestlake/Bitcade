b6861d1039ad3b5946f76a6d58ef303917a98dcc
danyul <danieltonyh06@gmail.com>
2024-05-23 21:08:51 -0700

    header changes
    stacked connect wallet button on top of balance. imported "arcade-classic" font

--- ui/src/Header.js
+++ ui/src/Header.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
-import WalletConnect from './components/WalletConnect';
+import React, { useState } from "react";
+import WalletConnect from "./components/WalletConnect";
 
 const Header = () => {
-  const [balance, setBalance] = useState('0.00');
+  const [balance, setBalance] = useState("0.00");
 
   return (
     <header className="header">
+      <head>
+      <link href="https://fonts.cdnfonts.com/css/arcade-classic" rel="stylesheet"/>
+      </head>
       <div className="header-left">Blocade</div>
-      <div className="header-middle">Balance: {balance} ETH</div>
+      <div className="header-logo">Logo</div>
       <div className="header-right">
-        <WalletConnect setBalance={setBalance} />
+        <div id="wallet-button">
+          <WalletConnect setBalance={setBalance} />
+        </div>
+
+        <div className="balance">Balance: {balance} ETH</div>
       </div>
     </header>
   );
